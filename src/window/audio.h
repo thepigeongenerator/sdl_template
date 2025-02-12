4025bd3585c47079e7d5ae7df696bf7b626142f1
unset <quinnthepigeon@gmail.com>
2025-02-12 17:44:15 +0100

    preform audio rework to allow for dynamic audio allocation upon new audio being played and also include the amount of miliseconds that a given fragment occupies

--- src/window/audio.h
+++ src/window/audio.h
@@ -4,20 +4,29 @@
 #include <stdint.h>
 
 typedef struct {
-    uint32_t length;
-    uint32_t mixed_amount;
-    uint8_t* buffer;
-} AudioData;
+    uint8_t* buf; // the audio buffer
+    uint32_t len; // length in bytes of the audio buffer
+    uint32_t ms;  // length in seconds of the audio buffer
+} audio_data;
+
+typedef struct audio_player {
+    struct audio_player* nxt; // pointer to the next audio fragment (can be NULL)
+    uint8_t* buf;             // pointer to the current
+    uint32_t len;             // length remaining of the audio buffer
+} audio_player;
 
 typedef struct {
-    SDL_AudioDeviceID id;
+    audio_player* audio_dat; // linked list of audio players
+    SDL_AudioDeviceID id;    // the audio device id
     int32_t freq;
-    SDL_AudioFormat format;
+    SDL_AudioFormat fmt;
     uint8_t channels;
-    AudioData* playing_audio;
-} AudioDevice;
+} audio_device;
 
 
-AudioData audio_load_wav(const AudioDevice* audio_device, const char* file_path);
-AudioDevice* audio_device_init(const int freq, const SDL_AudioFormat format, const uint8_t channels, const uint16_t samples);
-void audio_play(const AudioDevice* audio_device, const AudioData audio);
+audio_data audio_wav_load(audio_device const* audio_device, char const* file_path);
+audio_device* audio_device_init(int freq, SDL_AudioFormat format, uint8_t channels, uint16_t samples);
+void audio_play(audio_device* audio_device, audio_data const* audio);
+
+void audio_device_free(audio_device* dev);
+void audio_wav_unload(audio_data* dat);

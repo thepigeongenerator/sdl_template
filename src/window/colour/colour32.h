0b8564bbaabe6d7f0cf5a250dac865cd6d07e741
Quinn <quinnthepigeon@proton.me>
2025-02-12 17:58:01 +0100

    add more american macros

--- src/window/colour/colour32.h
+++ src/window/colour/colour32.h
@@ -11,7 +11,6 @@ typedef union {
         uint8_t r;
     };
 } colour32;
-#define color32 colour32
 
 #define COLOUR32_BLACK   ((colour32){0x000000FF})
 #define COLOUR32_RED     ((colour32){0xFF0000FF})
@@ -22,3 +21,16 @@ typedef union {
 #define COLOUR32_BLUE    ((colour32){0x0000FFFF})
 #define COLOUR32_MAGENTA ((colour32){0xFF00FFFF})
 #define COLOUR32_WHITE   ((colour32){0xFFFFFFFF})
+
+
+// american macros:
+#define color32         colour32
+#define COLOR32_BLACK   COLOUR32_BLACK
+#define COLOR32_RED     COLOUR32_RED
+#define COLOR32_YELLOW  COLOUR32_YELLOW
+#define COLOR32_ORANGE  COLOUR32_ORANGE
+#define COLOR32_GREEN   COLOUR32_GREEN
+#define COLOR32_CYAN    COLOUR32_CYAN
+#define COLOR32_BLUE    COLOUR32_BLUE
+#define COLOR32_MAGENTA COLOUR32_MAGENTA
+#define COLOR32_WHITE   COLOUR32_WHITE

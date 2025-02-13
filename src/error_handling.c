027bc85360f8214d7d70017570fbf6dc654d44bd
unset <quinnthepigeon@gmail.com>
2025-02-13 14:51:01 +0100

    implement / re-implement error-handling

---
+++ src/error_handling.c
@@ -0,0 +1,54 @@
+#include "error_handling.h"
+
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "SDL_messagebox.h"
+#include "main.h"
+
+#define PRINT_BUFFER_SIZE 128 // defines the buffer size for printing
+
+// writes the arguments to the specified buffer
+#define write_args(buf, fmt)                            \
+    va_list args;                                       \
+    va_start(args, fmt);                                \
+    (void)vsnprintf(buf, PRINT_BUFFER_SIZE, fmt, args); \
+    va_end(args);
+
+// sets the game status (in turn exiting (if not set to STATUS_RUNNING))
+static void stop(gamestatus status) {
+    set_gamestatus(status);
+}
+
+void debug(char const* fmt, ...) {
+    char const* env = getenv("DEBUG");
+    if (env == NULL || *env != '1')
+        return;
+
+    char buf[PRINT_BUFFER_SIZE] = {0};
+    write_args(buf, fmt);
+
+    (void)fprintf(stdout, "\033[95m%s\033[0m\n", buf);
+}
+
+void info(char const* fmt, ...) {
+    char buf[PRINT_BUFFER_SIZE] = {0};
+    write_args(buf, fmt);
+    (void)fprintf(stdout, "\033[0m%s\033[0m\n", buf); // write colour here for consistency
+}
+
+void warn(char const* fmt, ...) {
+    char buf[PRINT_BUFFER_SIZE] = {0};
+    write_args(buf, fmt);
+    (void)fprintf(stderr, "\033[93mW: %s\033[0m\n", buf);
+}
+
+void error(gamestatus status, char const* fmt, ...) {
+    char buf[PRINT_BUFFER_SIZE] = {0};
+    write_args(buf, fmt);
+    (void)fprintf(stderr, "\033[91mE: %s\033[0m\n", buf);
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "something went wrong! :O", buf, NULL);
+    stop(status);
+}

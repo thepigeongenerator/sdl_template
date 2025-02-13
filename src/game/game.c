ecdec2ab2a808420c4379b695bc2289b833929ab
Quinn <quinnthepigeon@proton.me>
2025-02-13 22:11:28 +0100

    implement rendering

--- src/game/game.c
+++ src/game/game.c
@@ -4,12 +4,12 @@
 
 void game_init(game_data* dat) {
     (void)dat;
-    error(STATUS_ERROR, "function not defined");
+    //error(STATUS_ERROR, "function not defined");
 }
 
 void game_update(game_data* dat) {
     (void)dat;
-    error(STATUS_ERROR, "function not defined");
+    //error(STATUS_ERROR, "function not defined");
 }
 
 void game_free(game_data* dat) {

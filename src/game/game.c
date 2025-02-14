178552c5c5031f2a7566c33c331a93b8a086dd42
unset <quinnthepigeon@gmail.com>
2025-02-14 19:27:30 +0100

    allow setting of game status & escape exits window

--- src/game/game.c
+++ src/game/game.c
@@ -1,5 +1,12 @@
 #include "game.h"
 
+#include <SDL_keyboard.h>
+#include <SDL_scancode.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "../error.h"
 #include "gametime.h"
 
 void game_init(gamedata* dat) {
@@ -10,6 +17,10 @@ void game_init(gamedata* dat) {
 
 void game_update(gamedata* dat) {
     gametime_update(&dat->time);
+    uint8_t const* keys = SDL_GetKeyboardState(NULL);
+
+    if (keys[SDL_SCANCODE_ESCAPE])
+        set_gamestatus(STATUS_SUCCESS);
 }
 
 void game_free(gamedata* dat) {
